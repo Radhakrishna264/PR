480e6e3033b47bf430daef57d81540270b73a960
Radhakrishna264 <radhakrishnan100806@gmail.com>
2026-01-15 14:34:31 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-
-  // 🔴 MOST IMPORTANT
-  output: "standalone",
-
-  // 🔴 Cloudflare Pages ke liye
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
